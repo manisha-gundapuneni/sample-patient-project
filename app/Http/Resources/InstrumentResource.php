688c6587e31f2d7ad054c86de6d49262d25c3953
manisha-gundapuneni <manisha.rg19@gmail.com>
2026-04-15 14:52:49 -0400

    Add files via upload

---
+++ app/Http/Resources/InstrumentResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class InstrumentResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'questions' => QuestionResource::collection($this->whenLoaded('questions')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
